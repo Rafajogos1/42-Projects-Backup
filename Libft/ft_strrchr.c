e81e10a1a793968c1324185ead12f94d443fa9bf
Rafael Martins paco ramiao gameiro <ramartin@c1r16s3.42lisboa.com>
2022-02-17 12:54:58 +0000

    Libft

---
+++ Libft/ft_strrchr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ramartin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 14:07:18 by ramartin          #+#    #+#             */
+/*   Updated: 2022/02/17 10:38:10 by ramartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	len;
+
+	len = ft_strlen((char *)s);
+	while (len != 0 && s[len] != (char)c)
+		len--;
+	if (s[len] == (char)c)
+		return ((char *)&s[len]);
+	return (NULL);
+}
