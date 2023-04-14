a201c7133999636fb347fb11be0202b41a7f0352
Rafael Martins paco ramiao gameiro <ramartin@c1r16s2.42lisboa.com>
2023-04-14 11:18:42 +0100

    Starting the main part of Philosophers.

--- Rank 03/Philosophers/philo/philosophers_threads.c
+++ Rank 03/Philosophers/philo/philosophers_threads.c
@@ -6,19 +6,32 @@
 /*   By: ramartin <ramartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 18:32:52 by rafael            #+#    #+#             */
-/*   Updated: 2023/04/10 18:20:18 by ramartin         ###   ########.fr       */
+/*   Updated: 2023/04/14 11:16:26 by ramartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+void	philo_death(int id, t_mutex *mutex)
+{
+	printf("%i %i died\n", id);
+}
+
 void	*philo_life_cycle(void *forks_pointer)
 {
 	t_mutex	*mutex;
 	int		id;
+	int		ended;
+	int		last_ate;
 
+	ended = 0;
 	mutex = (t_mutex *) forks_pointer;
 	id = mutex->philo_id;
 	printf("I'm philosopher %i.\n", id);
+	while(!ended)
+	{
+		ended = !ended;
+	}
+	printf("End %i.\n", id);
 	pthread_exit(NULL);
 }
